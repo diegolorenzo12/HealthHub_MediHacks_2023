34a69288adfca4af6c422ababcb4ffa7dbfa5600
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2023-09-09 10:31:14 -0700

    added firebase and changed from next to react

---
+++ Client/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration, replace it with your project keys
+
+const firebaseConfig = {
+  apiKey: process.env.firebaseapiKey,
+  authDomain: process.env.firebaseauthDomain,
+  databaseURL: process.env.firebasedatabaseURL,
+  projectId: process.env.firebaseprojectId,
+  storageBucket: process.env.firebasestorageBucket,
+  messagingSenderId: process.env.firebasemessagingSenderId,
+  appId: process.env.firebaseappId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
